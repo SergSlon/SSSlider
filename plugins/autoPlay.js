18e552c7f0a7dc5baac223a2a473b4cfc617e5b9
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-16 22:55:58 +0300

    add auto play plugin

---
+++ plugins/autoPlay.js
@@ -0,0 +1,147 @@
+/* * * *
+ * autoPlay
+ *
+ * this file is a part of SSSlider - jQuery modular slider
+ *
+ * Copyright (c) 2013 Sergei Lyamin
+ * https://github.com/SergSlon
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ * */
+
+/*
+ * EVENTS
+ * ▬ sss.player.init
+ * ▬ sss.player.play
+ * ▬ sss.player.stop
+ * ▬ sss.player.restart
+ * ▬ sss.player.pause
+ * ▬ sss.player.resume
+ * */
+
+;(function (win) {
+	"use strict";
+
+	$.extend($.fn.SSSlider.plugins, {
+		autoPlay: function (SSSlider, options) {
+
+			(function(SSSlider, options, win){
+				var plugin = {},
+					defaults = {
+						// [int] milliseconds
+						// time of displaying each slide
+						stepTime:2500,
+						// [bool] enable or disable auto play pause
+						// when hovering or touching the slide
+						pauseOnHover:false
+					};
+
+				plugin.options = $.extend({}, defaults, options);
+
+				plugin.player = function(stepTime){
+					var start,
+						remaining = stepTime,
+						self = this;
+
+					var playEvent = SSSlider.attachPreventableEvent('sss.player.init');
+					if(!playEvent)
+						return;
+
+					this.play = function() {
+						start = new Date();
+
+						SSSlider.playerID = win.setTimeout(function() {
+							remaining = stepTime;
+
+							var playEvent = SSSlider.attachPreventableEvent('sss.player.play');
+							if(!playEvent)
+								return;
+
+							self.play();
+							SSSlider.next();
+
+						}, remaining);
+					};
+
+					this.stop = function(){
+						var playEvent = SSSlider.attachPreventableEvent('sss.player.stop');
+						if(!playEvent)
+							return;
+
+						win.clearTimeout(SSSlider.playerID);
+					};
+
+					this.pause = function() {
+						var playEvent = SSSlider.attachPreventableEvent('sss.player.pause');
+						if(!playEvent)
+							return;
+
+						this.stop();
+						remaining -= new Date() - start;
+					};
+
+					this.restart = function(){
+						this.stop();
+						remaining = stepTime;
+						this.play();
+						SSSlider.$element.trigger('sss.player.restart');
+					};
+
+					this.getRemaining = function(){
+						return remaining;
+					}
+				};
+
+				plugin.checkPauseOnHover = function(){
+					if (plugin.options.pauseOnHover){
+						SSSlider.$element.one('mousemove touchmove',function () {
+							SSSlider.player.pause();
+						});
+
+						SSSlider.$element.on('mouseenter touchenter',function () {
+							SSSlider.player.pause();
+						}).on('mouseleave touchleave touchcancel', function () {
+							SSSlider.player.play();
+							SSSlider.$element.trigger('sss.player.resume');
+						});
+					}
+				};
+
+				plugin.runPlayer = function(){
+					var stepTime = Math.abs(plugin.options.stepTime);
+
+					if (stepTime > 0){
+
+						SSSlider.player = new plugin.player(plugin.options.stepTime);
+						SSSlider.player.play();
+						SSSlider.player.isPaused = false;
+
+						plugin.checkPauseOnHover();
+
+						SSSlider.$element.on('sss.player.pause', function(){
+							SSSlider.player.isPaused = true;
+						});
+
+						SSSlider.$element.on('sss.player.resume', function(){
+							SSSlider.player.isPaused = false;
+						});
+
+						// restart player when move
+						SSSlider.$element.on('sss.move', function(){
+							SSSlider.player.restart();
+							if(SSSlider.player.isPaused)
+								SSSlider.player.pause();
+						});
+
+					}
+				};
+
+				plugin.runPlayer();
+
+			}(SSSlider, options, win));
+
+		}
+	});
+
+})(window);
