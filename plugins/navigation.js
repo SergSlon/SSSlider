8841bafa943220d122bcdf76e083cfb515d42064
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-22 13:15:42 +0300

    change "navigation" plugin initialization

--- plugins/navigation.js
+++ plugins/navigation.js
@@ -14,49 +14,46 @@
 	"use strict";
 
 	$.extend($.fn.SSSlider.plugins, {
+
 		navigation: function (SSSlider, options) {
+			var plugin = this,
+				defaults = {
+					// [bool] enable or disable handler of the next slide button
+					nextButton: true,
+					// [bool] enable or disable handler of the previous slide button
+					prevButton: true,
 
-			(function (SSSlider, options) {
-				var plugin = {},
-					defaults = {
-						// [bool] enable or disable handler of the next slide button
-						nextButton: true,
-						// [bool] enable or disable handler of the previous slide button
-						prevButton: true,
-
-						selectors: {
-							// [string] next slide button selector
-							next: '.next',
-							// [string] prev slide button selector
-							prev: '.prev'
-						}
-					};
-
-				plugin.options = $.extend(true, {}, defaults, options);
-
-				plugin.enableNextButton = function () {
-					var $nextButton = SSSlider.findBySelector(plugin.options.selectors.next);
-					$nextButton.on('click touch', function () {
-						SSSlider.next();
-					});
+					selectors: {
+						// [string] next slide button selector
+						next: '.next',
+						// [string] prev slide button selector
+						prev: '.prev'
+					}
 				};
 
-				plugin.enablePrevButton = function () {
-					var $prevButton = SSSlider.findBySelector(plugin.options.selectors.prev);
-					$prevButton.on('click touch', function () {
-						SSSlider.prev();
-					});
-				};
+			plugin.options = $.extend(true, {}, defaults, options);
 
-				if (plugin.options.nextButton)
-					plugin.enableNextButton();
+			plugin.enableNextButton = function () {
+				var $nextButton = SSSlider.findBySelector(plugin.options.selectors.next);
+				$nextButton.on('click touch', function () {
+					SSSlider.next();
+				});
+			};
 
-				if (plugin.options.prevButton)
-					plugin.enablePrevButton();
+			plugin.enablePrevButton = function () {
+				var $prevButton = SSSlider.findBySelector(plugin.options.selectors.prev);
+				$prevButton.on('click touch', function () {
+					SSSlider.prev();
+				});
+			};
 
-			}(SSSlider, options, $));
+			if (plugin.options.nextButton)
+				plugin.enableNextButton();
+
+			if (plugin.options.prevButton)
+				plugin.enablePrevButton();
 
 		}
-	});
 
+	});
 })();
