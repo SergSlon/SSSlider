78a3ef0d487769e114efcb4c0e3d431c9b92e324
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-22 13:22:37 +0300

    change "progress bar" plugin initialization;

--- plugins/slideProgressBar.js
+++ plugins/slideProgressBar.js
@@ -22,66 +22,64 @@
 	"use strict";
 
 	$.extend($.fn.SSSlider.plugins, {
+
 		slideProgressBar: function (SSSlider, options) {
-			(function (SSSlider, options) {
-				var plugin = {},
-					defaults = {
-						// [bool] enable or disable slide progress bar
-						// ***require autoPlay plugin
-						slideBar: true,
+			var plugin = this,
+				defaults = {
+					// [bool] enable or disable slide progress bar
+					// ***require autoPlay plugin
+					slideBar: true,
 
-						selectors: {
-							// [string] slide progress bar selector
-							slide: '.slide-progress-bar',
-							// [string] filled area selector
-							filledArea: '.filled'
-						}
-					};
-
-				plugin.options = $.extend(true, {}, defaults, options);
-
-				plugin.slideBar = function () {
-					if (plugin.options.slideBar) {
-						if (!('autoPlay' in SSSlider.plugins))
-							return;
-
-						var progressBarS = plugin.options.selectors.slide,
-							filledAreaS = plugin.options.selectors.filledArea,
-							$bar = SSSlider.findBySelector(progressBarS + ' ' + filledAreaS);
-
-						var fillArea = function (time) {
-							$bar.animate({width: "100%"}, time, "linear");
-						};
-
-						var resetProgress = function () {
-							$bar.stop(true, true).width(0);
-						};
-
-						SSSlider.$element.on('sss.player.pause', function () {
-							$bar.stop(true);
-						});
-
-						SSSlider.$element.on('sss.move', function () {
-							if (SSSlider.player.isPaused)
-								resetProgress();
-						});
-
-						SSSlider.$element.on('sss.player.init sss.player.play sss.player.resume sss.player.restart', function (e) {
-							var stepTime = SSSlider.player.getRemaining();
-
-							if (!(e.type == 'sss' && e.namespace == 'player.resume'))
-								resetProgress();
-
-							fillArea(stepTime);
-						});
+					selectors: {
+						// [string] slide progress bar selector
+						slide: '.slide-progress-bar',
+						// [string] filled area selector
+						filledArea: '.filled'
 					}
 				};
 
-				plugin.slideBar();
+			plugin.options = $.extend(true, {}, defaults, options);
 
-			}(SSSlider, options));
+			plugin.slideBar = function () {
+				if (plugin.options.slideBar) {
+					if (!('autoPlay' in SSSlider.plugins))
+						return;
+
+					var progressBarS = plugin.options.selectors.slide,
+						filledAreaS = plugin.options.selectors.filledArea,
+						$bar = SSSlider.findBySelector(progressBarS + ' ' + filledAreaS);
+
+					var fillArea = function (time) {
+						$bar.animate({width: "100%"}, time, "linear");
+					};
+
+					var resetProgress = function () {
+						$bar.stop(true, true).width(0);
+					};
+
+					SSSlider.$element.on('sss.player.pause', function () {
+						$bar.stop(true);
+					});
+
+					SSSlider.$element.on('sss.move', function () {
+						if (SSSlider.player.isPaused)
+							resetProgress();
+					});
+
+					SSSlider.$element.on('sss.player.init sss.player.play sss.player.resume sss.player.restart', function (e) {
+						var stepTime = SSSlider.player.getRemaining();
+
+						if (!(e.type == 'sss' && e.namespace == 'player.resume'))
+							resetProgress();
+
+						fillArea(stepTime);
+					});
+				}
+			};
+
+			plugin.slideBar();
 
 		}
-	});
 
+	});
 })();
