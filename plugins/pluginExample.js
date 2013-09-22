7a6ee09bd0c5e4770ae6352e34768363f8e0fa86
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-22 13:19:52 +0300

    change "plugin example" plugin  initialization;

--- plugins/pluginExample.js
+++ plugins/pluginExample.js
@@ -14,52 +14,50 @@
 	"use strict";
 
 	$.extend($.fn.SSSlider.plugins, {
+
 		// plugin name
 		pluginExample: function (SSSlider, options) {
+			var plugin = this,
 
-			(function(SSSlider, options){
-				var plugin = {},
-
-					// default options
-					defaults = {
-						pluginOption: 'pluginOptionValue'
-					};
-
-				// plugin options
-				// use
-				//     plugin.options = $.extend(true, {}, defaults, options);
-				// for deep copy
-				plugin.options = $.extend({}, defaults, options);
-
-				// add new SSSlider method
-				// or
-				// completely change SSSlider method
-				SSSlider.methodName = function () {
-					console.log('pluginName methodName called');
+				// default options
+				defaults = {
+					pluginOption: 'pluginOptionValue'
 				};
 
-				// add new plugin method
-				plugin.method = function(){
-					//method code
+			// plugin options
+			// use
+			//     plugin.options = $.extend(true, {}, defaults, options);
+			// for deep copy
+			plugin.options = $.extend({}, defaults, options);
+
+			// add new SSSlider method
+			// or
+			// completely change SSSlider method
+			SSSlider.methodName = function () {
+				console.log('pluginName methodName called');
+			};
+
+			// add new plugin method
+			plugin.method = function(){
+				//method code
+			};
+
+			// add new functionality to SSSlider methods by extending them
+			SSSlider.next = (function (fn) {
+				return function () {
+					fn.call(this);
+					console.log('next called');
 				};
+			})(SSSlider.next);
 
-				// add new functionality to SSSlider methods by extending them
-				SSSlider.next = (function (fn) {
-					return function () {
-						fn.call(this);
-						console.log('next called');
-					};
-				})(SSSlider.next);
-
-				//events usage
-				SSSlider.$element.on('sss.init', function () {
-					console.group("SSSlider");
-					console.log("Hello world !!! ");
-					console.groupEnd()
-				});
-
-			}(SSSlider, options));
+			//events usage
+			SSSlider.$element.on('sss.init', function () {
+				console.group("SSSlider");
+				console.log("Hello world !!! ");
+				console.groupEnd()
+			});
 
 		}
+
 	});
 })();
