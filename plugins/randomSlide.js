3e58772cb37bb921712602c4ed5f21a831178028
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-22 13:21:37 +0300

    change "random slide" plugin initialization;

--- plugins/randomSlide.js
+++ plugins/randomSlide.js
@@ -14,83 +14,80 @@
 	"use strict";
 
 	$.extend($.fn.SSSlider.plugins, {
+
 		randomSlide: function (SSSlider, options) {
+			var plugin = this,
+				defaults = {
+					// [bool] enable or disable random slide (next or prev slide will be chosen randomly)
+					alwaysRandom: true,
+					// [bool] enable or disable random slide button
+					randomSlideButton: false,
 
-			(function (SSSlider, options) {
-				var plugin = {},
-					defaults = {
-						// [bool] enable or disable random slide (next or prev slide will be chosen randomly)
-						alwaysRandom: true,
-						// [bool] enable or disable random slide button
-						randomSlideButton: false,
+					selectors: {
+						// [string] random slide button selector
+						random: '.random-slide'
+					}
+				};
 
-						selectors: {
-							// [string] random slide button selector
-							random: '.random-slide'
-						}
-					};
+			plugin.options = $.extend(true, {}, defaults, options);
 
-				plugin.options = $.extend(true, {}, defaults, options);
+			plugin.getRandomSlide = function () {
+				var slidesCount = SSSlider.$slides.length,
+					numberOfLastIndexesToRemember = Math.floor(slidesCount / 2),
+					current = SSSlider.getActiveSlideIndex(),
+					newIndex;
 
-				plugin.getRandomSlide = function () {
-					var slidesCount = SSSlider.$slides.length,
-						numberOfLastIndexesToRemember = Math.floor(slidesCount / 2),
-						current = SSSlider.getActiveSlideIndex(),
-						newIndex;
+				this.getRandomIndex = function () {
+					return (Math.floor(Math.random() * slidesCount))
+				};
 
-					this.getRandomIndex = function () {
-						return (Math.floor(Math.random() * slidesCount))
-					};
+				this.getLastIndexes = function () {
+					this.lastIndexes = this.lastIndexes || [current];
+					if (this.lastIndexes.length > numberOfLastIndexesToRemember)
+						this.lastIndexes.length = numberOfLastIndexesToRemember;
+				};
 
-					this.getLastIndexes = function () {
-						this.lastIndexes = this.lastIndexes || [current];
-						if (this.lastIndexes.length > numberOfLastIndexesToRemember)
-							this.lastIndexes.length = numberOfLastIndexesToRemember;
-					};
-
-					this.getNewIndex = function () {
+				this.getNewIndex = function () {
+					newIndex = this.getRandomIndex();
+					this.getLastIndexes();
+					// if new index isn't in the last indexes array
+					while ($.inArray(newIndex, this.lastIndexes) !== -1) {
 						newIndex = this.getRandomIndex();
-						this.getLastIndexes();
-						// if new index isn't in the last indexes array
-						while ($.inArray(newIndex, this.lastIndexes) !== -1) {
-							newIndex = this.getRandomIndex();
-						}
-						this.lastIndexes.unshift(newIndex);
-						return newIndex;
+					}
+					this.lastIndexes.unshift(newIndex);
+					return newIndex;
+				};
+
+				newIndex = this.getNewIndex();
+
+				return(newIndex);
+			};
+
+			plugin.enableRandomSlideButton = function () {
+				var $randomButton = SSSlider.findBySelector(plugin.options.selectors.random),
+					newIndex;
+
+				$randomButton.on('click touch', function () {
+					newIndex = plugin.getRandomSlide();
+					SSSlider.moveTo(newIndex);
+				});
+			};
+
+			plugin.enableRandomSlide = function () {
+				SSSlider.getFutureSlideIndex = (function (fn) {
+					return function () {
+						return plugin.getRandomSlide();
 					};
+				})(SSSlider.getFutureSlideIndex);
+			};
 
-					newIndex = this.getNewIndex();
+			if (plugin.options.alwaysRandom)
+				plugin.enableRandomSlide();
 
-					return(newIndex);
-				};
-
-				plugin.enableRandomSlideButton = function () {
-					var $randomButton = SSSlider.findBySelector(plugin.options.selectors.random),
-						newIndex;
-
-					$randomButton.on('click touch', function () {
-						newIndex = plugin.getRandomSlide();
-						SSSlider.moveTo(newIndex);
-					});
-				};
-
-				plugin.enableRandomSlide = function () {
-					SSSlider.getFutureSlideIndex = (function (fn) {
-						return function () {
-							return plugin.getRandomSlide();
-						};
-					})(SSSlider.getFutureSlideIndex);
-				};
-
-				if (plugin.options.alwaysRandom)
-					plugin.enableRandomSlide();
-
-				if (plugin.options.randomSlideButton)
-					plugin.enableRandomSlideButton();
-
-			}(SSSlider, options));
+			if (plugin.options.randomSlideButton)
+				plugin.enableRandomSlideButton();
 
 		}
-	});
 
+	});
 })();
