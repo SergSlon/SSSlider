988f544a48bbbe606a9014395a61de02987c65ed
Sergei Lyamin (Liamin) <liamin.web@gmail.com>
2013-09-22 13:24:51 +0300

    change "slider progress bar" plugin initialization;

--- plugins/sliderProgressBar.js
+++ plugins/sliderProgressBar.js
@@ -15,47 +15,44 @@
 	"use strict";
 
 	$.extend($.fn.SSSlider.plugins, {
+
 		sliderProgressBar: function (SSSlider, options) {
+			var plugin = this,
+				defaults = {
+					// [bool] enable or disable progress bar for whole slider
+					sliderBar: true,
 
-			(function (SSSlider, options) {
-				var plugin = {},
-					defaults = {
-						// [bool] enable or disable progress bar for whole slider
-						sliderBar: true,
-
-						selectors: {
-							// [string] slider progress bar selector
-							slider: '.slider-progress-bar',
-							// [string] filled area class name
-							filledArea: '.filled'
-						}
-					};
-
-				plugin.options = $.extend(true, {}, defaults, options);
-
-				plugin.sliderBar = function () {
-					if (plugin.options.sliderBar) {
-						var slidesCount = SSSlider.$slides.length,
-							slideFilledArea = Math.abs(100 / slidesCount),
-							progressBarS = plugin.options.selectors.slider,
-							filledAreaS = plugin.options.selectors.filledArea,
-							$bar = SSSlider.findBySelector(progressBarS + ' ' + filledAreaS),
-							filled;
-
-						SSSlider.$element.on('sss.afterChangeSlide sss.init', function () {
-							var currentSlideNumber = SSSlider.$slides.index(SSSlider.$activeSlide) + 1;
-							filled = currentSlideNumber * slideFilledArea;
-
-							$bar.css('width', filled + '%');
-						});
+					selectors: {
+						// [string] slider progress bar selector
+						slider: '.slider-progress-bar',
+						// [string] filled area class name
+						filledArea: '.filled'
 					}
 				};
 
-				plugin.sliderBar();
+			plugin.options = $.extend(true, {}, defaults, options);
 
-			}(SSSlider, options));
+			plugin.sliderBar = function () {
+				if (plugin.options.sliderBar) {
+					var slidesCount = SSSlider.$slides.length,
+						slideFilledArea = Math.abs(100 / slidesCount),
+						progressBarS = plugin.options.selectors.slider,
+						filledAreaS = plugin.options.selectors.filledArea,
+						$bar = SSSlider.findBySelector(progressBarS + ' ' + filledAreaS),
+						filled;
+
+					SSSlider.$element.on('sss.afterChangeSlide sss.init', function () {
+						var currentSlideNumber = SSSlider.$slides.index(SSSlider.$activeSlide) + 1;
+						filled = currentSlideNumber * slideFilledArea;
+
+						$bar.css('width', filled + '%');
+					});
+				}
+			};
+
+			plugin.sliderBar();
 
 		}
-	});
 
+	});
 })();
